1d04a9f9beb134e1eb12e5faeed25d2e0cb755df
Joerg Thomas <j.thomas@peaknx.com>
2023-08-30 18:53:05 +0200

    Register FormattableTextEditor.

---
+++ FormattableTextEditor/FontsHelperFTE.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichTextEditor
+{
+    public static class FontsHelperFTE
+    {
+        public static List<String> FontAliasList = new List<String>();
+
+        public static List<string> GetFontAliasList ()
+        {
+            InitFontAliasList();
+            return FontAliasList;
+        }
+        public static string GetCurrentFontString(string value)
+        {
+            var fonts = GetFontstrings();
+            var font = "";  
+            foreach (var f in fonts)
+            {
+                if (f.Value == value)
+                {
+                    font = f.Key;
+                    break;
+                }
+            }
+
+            return font;
+        }
+
+        private static Dictionary<string, string> GetFontstrings()
+        {
+            var fonts = new Dictionary<string, string>();
+
+            fonts.Add("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.Add("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            fonts.Add("IWantMyTtr-ALV8p.ttf", "IWantMy");
+            fonts.Add("BarbieDoll-p7Yqd.ttf", "BarbieDoll");
+            fonts.Add("Humayra-3z4mp.ttf", "Humayra");
+            fonts.Add("TypoRgbOutlinedemo-Ean9W.otf", "TypeRgb");
+            fonts.Add("Aqira-Wyg0n.ttf", "Aqira");
+            fonts.Add("Harima-X3od9.otf", "Harima");
+            fonts.Add("Hanking-1GgZZ.otf", "Hanking");
+            fonts.Add("Haidenisa-rgMdx.otf", "Haidenisa");
+            fonts.Add("DinoRegular-X3pwg.ttf", "Dino");
+            fonts.Add("PamelloGlenstar-GORZP.otf", "Pamello");
+            fonts.Add("Rampwind-WygP9.otf", "Rampwind");
+            fonts.Add("SuperHappyDemo-MVKBp.ttf", "SuperHappy");
+            fonts.Add("Hagi-MVKOP.otf", "Hagi");
+            fonts.Add("BakeryHolland-rgM8B.ttf", "BakeryHolland");
+            fonts.Add("SpicyNachos-BWJqw.ttf", "SpicyNachos");
+            fonts.Add("FunkyFoodieDoodie-DOlG3.ttf", "FunkyFoodieDoodie");
+            fonts.Add("SpicyHotdog-gxVpE.ttf", "SpicyHotdog");
+            fonts.Add("Robienz-8M94n.ttf", "Robienz");
+            fonts.Add("AmericandreamsRegular-1Gg2v.otf", "Americandreams");
+            fonts.Add("Banetty-lg1Kw.otf", "Banetty");
+            fonts.Add("Fortieth-7BarD.ttf", "Fortieth");
+            fonts.Add("Cleudia-JRYpj.otf", "Cleudia");
+            fonts.Add("Home888-ALVRM.otf", "Home888");
+            fonts.Add("Shirlock-rgMgy.otf", "Shirlock");
+            fonts.Add("ShortimeSunday-9Ylx5.ttf", "ShortimeSunday");
+            fonts.Add("Signspaintedscript-owLJB.ttf", "Signspaintedscript");
+            fonts.Add("Badgline-gxKOq.otf", "Badgline");
+            fonts.Add("Doknatle-d9YRE.otf", "Doknatle");
+            fonts.Add("Blanvad-mLdwa.otf", "Blanvad");
+            fonts.Add("ChorneilRegular-7BVJR.otf", "ChorneilRegular");
+            fonts.Add("Travesty-K7XmZ.ttf", "Travesty");
+            fonts.Add("MadfoolRegular-lgm2V.ttf", "MadfoolRegular");
+            fonts.Add("OnelySans-7BVzl.ttf", "OnelySans");
+            fonts.Add("Malonet-vmWYO.otf", "Malonet");
+            fonts.Add("NightScreamPersonalUse-OV0xo.otf", "NightScreamPersonalUse");
+            fonts.Add("ScaryNotesPersonalUse-ZVAxl.otf", "ScaryNotesPersonalUse");
+
+            return fonts;
+        }
+
+        private static void InitFontAliasList()
+        {
+            FontAliasList.Add("OpenSansRegular");
+            FontAliasList.Add("OpenSansSemibold");
+            FontAliasList.Add("IWantMy");
+            FontAliasList.Add("BarbieDoll");
+            FontAliasList.Add("Humayra");
+            FontAliasList.Add("TypeRgb");
+            FontAliasList.Add("Aqira");
+            FontAliasList.Add("Harima");
+            FontAliasList.Add("Hanking");
+            FontAliasList.Add("Dino");
+            FontAliasList.Add("Pamello");
+            FontAliasList.Add("Rampwind");
+            FontAliasList.Add("SuperHappy");
+            FontAliasList.Add("Hagi");
+            FontAliasList.Add("BakeryHolland");
+            FontAliasList.Add("SpicyNachos");
+            FontAliasList.Add("FunkyFoodieDoodie");
+            FontAliasList.Add("SpicyHotdog");
+            FontAliasList.Add("Robienz");
+            FontAliasList.Add("Americandreams");
+            FontAliasList.Add("Banetty");
+            FontAliasList.Add("Fortieth");
+            FontAliasList.Add("Home888");
+            FontAliasList.Add("Shirlock");
+            FontAliasList.Add("ShortimeSunday");
+            FontAliasList.Add("Signspaintedscript");
+            FontAliasList.Add("Badgline");
+            FontAliasList.Add("Doknatle");
+            FontAliasList.Add("Blanvad");
+            FontAliasList.Add("ChorneilRegular");
+            FontAliasList.Add("Travesty");
+            FontAliasList.Add("MadfoolRegular");
+            FontAliasList.Add("OnelySans");
+            FontAliasList.Add("Malonet");
+            FontAliasList.Add("NightScreamPersonalUse");
+            FontAliasList.Add("ScaryNotesPersonalUse");
+        }
+    }
+}
---
+++ FormattableTextEditor/HtmlEditor.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using FormattableTextEditor;
+using Microsoft.Maui.Controls;
+
+namespace RichTextEditor
+{
+	public class HtmlEditor : Editor, INotifyPropertyChanged
+	{
+		public static StyleBar StaticStyleBar = new StyleBar();
+
+		public event EventHandler HtmlRequested;
+		public event EventHandler<HtmlArgs> HtmlSet;
+		public event EventHandler<StyleArgs> StyleChangeRequested;
+		public event EventHandler<SelectionArgs> SelectionChangeHandler;
+
+		string HtmlString;
+		public int SelectionStart;
+		public int SelectionEnd;
+		private StyleArgsTransient transient;
+		
+
+        public static readonly BindableProperty FormattedTextProperty =
+        BindableProperty.Create(nameof(FormattedText), typeof(string), typeof(HtmlEditor), "", BindingMode.TwoWay);
+
+		public static readonly BindableProperty CanSetHtmlTextProperty =
+		  BindableProperty.Create(nameof(CanSetHtmlText), typeof(bool), typeof(HtmlEditor), false);
+
+		public string FormattedText
+        {
+            get => (string)GetValue(FormattedTextProperty);
+            set => SetValue(FormattedTextProperty, value);
+        }
+		public bool CanSetHtmlText
+		{
+			get => (bool)GetValue(CanSetHtmlTextProperty);
+			set => SetValue(CanSetHtmlTextProperty, value);
+		}
+
+		public HtmlEditor()
+		{
+			this.transient = StyleArgsTransientInstance.StyleArgsTransient;
+
+            this.PropertyChanged += (sender, e) => 
+			{
+				this.InvalidateMeasure();
+			};
+            this.transient.ChangeStyleArgsEvent += Transient_ChangeStyleArgsEvent;
+            this.StyleChangeRequested += HtmlEditor_StyleChangeRequested;
+			
+		}
+
+        private void HtmlEditor_StyleChangeRequested(object sender, StyleArgs e)
+        {
+            if (e.Style == "transfertext")
+			{
+				FormattedText = GetHtmlText();
+			}
+        }
+
+        private void Transient_ChangeStyleArgsEvent(object sender, StyleArgs e)
+        {
+            StyleChangeRequested(this, e);
+        }
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+			
+        }
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			base.OnPropertyChanged(propertyName);
+			if (propertyName == null) return;
+
+			if (propertyName == nameof(CanSetHtmlText))
+			{
+				if (CanSetHtmlText)
+				{
+                    FormattedText = GetHtmlText();
+					CanSetHtmlText = false;
+                }
+					
+			}
+		}
+
+		public class SelectionArgs : EventArgs
+		{
+			public int Start;
+			public int End;
+			public SelectionArgs(int start, int end)
+			{
+				Start = start;
+				End = end;
+			}
+		}
+
+		public void SetSelection(int start, int end)
+		{
+			var args = new SelectionArgs(start, end);
+			SelectionChangeHandler(this, args);
+		}
+
+		public void SetHtmlText(string htmlString)
+		{
+			HtmlString = htmlString;
+			var args = new HtmlArgs(htmlString);
+			if(HtmlSet != null) 
+			    HtmlSet(this, args);
+		}
+
+		public class HtmlArgs : EventArgs
+		{
+			public string HtmlToPass;
+			public HtmlArgs(string htmlToPass)
+			{
+				HtmlToPass = htmlToPass;
+			}
+		}
+
+		public string GetHtmlText()
+		{
+			if(HtmlRequested != null)
+			   HtmlRequested(this, new EventArgs());
+			return HtmlString;
+		}
+
+		public void BoldChanged()
+		{
+			StyleChangeRequested(this, new StyleArgs("bold"));
+		}
+
+		public void ItalicChanged()
+		{
+			StyleChangeRequested(this, new StyleArgs("italic"));
+		}
+
+		public void UnderlineChanged()
+		{
+			StyleChangeRequested(this, new StyleArgs("underline"));
+		}
+
+		public class StyleArgs : EventArgs
+		{
+			public string Style;
+			public StyleArgs(string style)
+			{
+				Style = style;
+			}
+		}
+		/*
+		bool CanPaste()
+		{
+			return false;
+		}*/
+
+
+	}
+}
